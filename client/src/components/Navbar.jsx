67b1930d182ece51928e0051d14e8aaafae11dfb
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-13 16:00:40 +0000

    Logout functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,12 +5,16 @@ import Home from './Home';
 import Signup from './Signup';
 
 export default function Navbar(props){
+    const logout = () => {
+        localStorage.removeItem("token");
+    }
     return(
         <div>
             <nav>
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/login'>Login</NavLink>
                 <NavLink to='/signup'>Signup</NavLink>
+                <button onClick={logout}>Logout</button>
             </nav>
             <main>
                 <Route exact path='/' component={Home} />
