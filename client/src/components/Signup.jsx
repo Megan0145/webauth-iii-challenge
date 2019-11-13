2ae1085b1211cf8f74866e5d38738224d66e7969
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-13 15:53:37 +0000

    Register account front end functionality

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,9 +1,27 @@
 import React, { useRef } from "react";
+import axios from "axios";
 
 export default function Signup(props) {
   const username = useRef(null);
   const password = useRef(null);
   const department = useRef(null);
+
+  const signup = e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:4500/api/register", {
+        username: username.current.value,
+        password: password.current.value,
+        department: department.current.value
+      })
+      .then(res => {
+        alert("Account Created! You can now log in");
+        props.history.push("/login");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   return (
     <div>
@@ -12,7 +30,7 @@ export default function Signup(props) {
         <input placeholder="Username" ref={username} type="text" />
         <input placeholder="Password" ref={password} type="password" />
         <input placeholder="Department" ref={department} type="text" />
-        <button>Sign Me Up</button>
+        <button onClick={signup}>Sign Me Up</button>
       </form>
     </div>
   );
