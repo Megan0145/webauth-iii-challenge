3be4220cfd97960127653c535d3216fac42ba2e8
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-13 15:58:20 +0000

    Front end sign in functionality, sets token to local storage on successful login, redirects user to homepage displaying users in same department

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,25 @@
 import React, { useRef } from "react";
+import axios from "axios";
 
 export default function Login(props) {
   const username = useRef(null);
   const password = useRef(null);
+
+  const login = e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:4500/api/login", {
+        username: username.current.value,
+        password: password.current.value
+      })
+      .then(res => {
+        localStorage.setItem("token", res.data.token);
+        props.history.push("/");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   return (
     <div>
@@ -10,7 +27,7 @@ export default function Login(props) {
       <form>
         <input placeholder="Username" ref={username} type="text" />
         <input placeholder="Password" ref={password} type="password" />
-        <button>Login</button>
+        <button onClick={login}>Login</button>
       </form>
     </div>
   );
